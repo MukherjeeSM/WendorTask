2873fe3964a784990a9f4136d9dd206bf3f4f5c4
SMukherjee <mukherjeesm10@gmail.com>
2018-04-26 13:51:59 +0530

    Basic orders api with body parsing

--- app.js
+++ app.js
@@ -1,9 +1,46 @@
 const express =  require ('express');
 const app=express();
+const morgan=require('morgan');
+const bodyParser=require('body-parser');
+
+
+
+const orderRoutes = require('./api/routes/orders');
+
+
+app.use(morgan('dev')); //logger middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 app.use((req,res,next)=>{
-	res.status(200).json({
-			message: 'Hello Shreya'
-	});
+	res.header("Access-Control-Allow-Origin","*");
+	res.header("Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Access, Authorization"
+		);
+	if(req.method==='OPTIONS'){
+		res.header('Access-Control-Allow-Methods','POST, PUT, PATCH, DELETE, GET');
+		return res.status(200).json({});
+	};
+	next();
 });
 
+
+app.use('/orders', orderRoutes);
+
+app.use((req,res,next)=>{
+	const error = new Error('Not Found');
+	error.status=404;
+	next(error);
+});
+
+
+app.use((error,req,res,next)=>{
+	res.status(error.status || 500);
+	res.json({
+		error: {
+			message: error.message
+		}
+	})
+});
 module.exports= app;
+
